2775199e9f8e59219593a0730b93759306aa1993
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2025-01-14 17:00:40 +0900

    [iOS] FEAT: iOS 프로젝트 초기 설정 #11

---
+++ src/iOS/KickTube/KickTube/KickTubeTests/KickTubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  KickTubeTests.swift
+//  KickTubeTests
+//
+//  Created by 김수경 on 1/14/25.
+//
+
+import Testing
+@testable import KickTube
+
+struct KickTubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
