676fdfbbef85e3f03105c5b61f2fa014ed42c2ee
dhwoo <wooda01@naver.com>
2025-01-21 18:10:57 +0900

    [BE] FEAT: 엔티티에 annotation 추가 #41

--- src/backend/main-server/main/src/main/java/com/kickzo/main/entity/Room.java
+++ src/backend/main-server/main/src/main/java/com/kickzo/main/entity/Room.java
@@ -14,13 +14,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "room")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Room {
 
 	@Id
