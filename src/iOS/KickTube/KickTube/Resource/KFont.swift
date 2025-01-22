ded124bd09e518dfef01fed72d9eb371bec812b8
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2025-01-22 15:03:05 +0900

    [iOS] FEAT: 로그인 화면 구현 #37
    - ReactorKit 적용

---
+++ src/iOS/KickTube/KickTube/Resource/KFont.swift
@@ -0,0 +1,56 @@
+//
+//  KFont.swift
+//  KickTube
+//
+//  Created by 김수경 on 1/21/25.
+//
+
+import UIKit
+
+struct KFont {
+    // MARK: - light
+
+    static let light10 = UIFont.systemFont(ofSize: 10, weight: .light)
+    static let light11 = UIFont.systemFont(ofSize: 11, weight: .light)
+    static let light12 = UIFont.systemFont(ofSize: 12, weight: .light)
+    static let light13 = UIFont.systemFont(ofSize: 13, weight: .light)
+    static let light14 = UIFont.systemFont(ofSize: 14, weight: .light)
+    static let light15 = UIFont.systemFont(ofSize: 15, weight: .light)
+    static let light16 = UIFont.systemFont(ofSize: 16, weight: .light)
+    static let light17 = UIFont.systemFont(ofSize: 17, weight: .light)
+    static let light18 = UIFont.systemFont(ofSize: 18, weight: .light)
+    static let light19 = UIFont.systemFont(ofSize: 19, weight: .light)
+    static let light20 = UIFont.systemFont(ofSize: 20, weight: .light)
+    
+    
+    // MARK: - middle
+
+    static let middle10 = UIFont.systemFont(ofSize: 10, weight: .medium)
+    static let middle11 = UIFont.systemFont(ofSize: 11, weight: .medium)
+    static let middle12 = UIFont.systemFont(ofSize: 12, weight: .medium)
+    static let middle13 = UIFont.systemFont(ofSize: 13, weight: .medium)
+    static let middle14 = UIFont.systemFont(ofSize: 14, weight: .medium)
+    static let middle15 = UIFont.systemFont(ofSize: 15, weight: .medium)
+    static let middle16 = UIFont.systemFont(ofSize: 16, weight: .medium)
+    static let middle17 = UIFont.systemFont(ofSize: 17, weight: .medium)
+    static let middle18 = UIFont.systemFont(ofSize: 18, weight: .medium)
+    static let middle19 = UIFont.systemFont(ofSize: 19, weight: .medium)
+    static let middle20 = UIFont.systemFont(ofSize: 20, weight: .medium)
+    static let middle21 = UIFont.systemFont(ofSize: 21, weight: .medium)
+    static let middle22 = UIFont.systemFont(ofSize: 22, weight: .medium)
+    
+    
+    // MARK: - bold
+
+    static let bold10 = UIFont.systemFont(ofSize: 10, weight: .bold)
+    static let bold11 = UIFont.systemFont(ofSize: 11, weight: .bold)
+    static let bold12 = UIFont.systemFont(ofSize: 12, weight: .bold)
+    static let bold13 = UIFont.systemFont(ofSize: 13, weight: .bold)
+    static let bold14 = UIFont.systemFont(ofSize: 14, weight: .bold)
+    static let bold15 = UIFont.systemFont(ofSize: 15, weight: .bold)
+    static let bold16 = UIFont.systemFont(ofSize: 16, weight: .bold)
+    static let bold17 = UIFont.systemFont(ofSize: 17, weight: .bold)
+    static let bold18 = UIFont.systemFont(ofSize: 18, weight: .bold)
+    static let bold19 = UIFont.systemFont(ofSize: 19, weight: .bold)
+    static let bold20 = UIFont.systemFont(ofSize: 20, weight: .bold)
+}
