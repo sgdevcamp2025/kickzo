0f27944b869b0d5d1338789f8cfa57599c41a02a
juwon5272 <jackson5272@naver.com>
2025-01-22 10:05:11 +0900

    [FE] FEAT: 영상의 화살표를 누르면 플레이리스트 순서 변경 #47

--- src/frontend/src/components/sidebar/playlist/index.tsx
+++ src/frontend/src/components/sidebar/playlist/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import Trashcan from '@/assets/img/Trashcan.svg';
+import ArrowUp from '@/assets/img/ArrowUp.svg';
+import ArrowDown from '@/assets/img/ArrowDown.svg';
 import { CommonButton } from '@/components/common/button';
 import { Container, Wrapper, VideoItem, Thumbnail, InputContainer, SearchInput } from './index.css';
 import { ButtonColor } from '@/types/enums/ButtonColor';
@@ -19,6 +21,7 @@ interface IPlaylist {
 
 export const Playlist = (props: IPlaylist) => {
   const [inputUrl, setInputUrl] = useState<string>('');
+  const [draggedIndex, setDraggedIndex] = useState<number | null>(null);
 
   const extractVideoIdAndStartTime = (url: string) => {
     const regex =
@@ -42,30 +45,72 @@ export const Playlist = (props: IPlaylist) => {
       {
         id: videoId,
         start: startTime,
-        thumbnail: `https://img.youtube.com/vi/${videoId}/0.jpg`, // NOTE - 유튜브 썸네일
+        thumbnail: `https://img.youtube.com/vi/${videoId}/0.jpg`,
       },
     ]);
     setInputUrl('');
   };
 
-  // const handleVideoStart = (index: number) => {
-  //   props.setVideoQueue(prevQueue => prevQueue.filter((_, i) => i !== index));
-
-  //   if (props.currentIndex === index) {
-  //     props.setCurrentIndex(0);
-  //   } else if (props.currentIndex > index) {
-  //     props.setCurrentIndex(props.currentIndex - 1);
-  //   }
-  // };
-
   const handleRemove = (index: number) => {
     props.setVideoQueue(prevQueue => prevQueue.filter((_, i) => i !== index));
-
     if (props.currentIndex === index) {
       props.setCurrentIndex(0);
     } else if (props.currentIndex > index) {
       props.setCurrentIndex(props.currentIndex - 1);
     }
+  };
+
+  const handleMoveUp = (index: number) => {
+    if (index === 0) return;
+
+    const newQueue = [...props.videoQueue];
+    const temp = newQueue[index];
+    newQueue[index] = newQueue[index - 1];
+    newQueue[index - 1] = temp;
+
+    props.setVideoQueue(newQueue);
+
+    if (props.currentIndex === index) {
+      props.setCurrentIndex(index - 1);
+    } else if (props.currentIndex === index - 1) {
+      props.setCurrentIndex(index);
+    }
+  };
+
+  const handleMoveDown = (index: number) => {
+    if (index === props.videoQueue.length - 1) return;
+
+    const newQueue = [...props.videoQueue];
+    const temp = newQueue[index];
+    newQueue[index] = newQueue[index + 1];
+    newQueue[index + 1] = temp;
+
+    props.setVideoQueue(newQueue);
+
+    if (props.currentIndex === index) {
+      props.setCurrentIndex(index + 1);
+    } else if (props.currentIndex === index + 1) {
+      props.setCurrentIndex(index);
+    }
+  };
+
+  const handleDragStart = (index: number) => {
+    setDraggedIndex(index);
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (index: number) => {
+    if (draggedIndex === null || draggedIndex === index) return;
+
+    const updatedQueue = [...props.videoQueue];
+    const [draggedItem] = updatedQueue.splice(draggedIndex, 1);
+    updatedQueue.splice(index, 0, draggedItem);
+
+    props.setVideoQueue(updatedQueue);
+    setDraggedIndex(null);
   };
 
   return (
@@ -74,28 +119,56 @@ export const Playlist = (props: IPlaylist) => {
         {props.videoQueue.map((video, index) => (
           <VideoItem
             key={`${video.id}-${index}`}
+            draggable
+            onDragStart={() => handleDragStart(index)}
+            onDragOver={handleDragOver}
+            onDrop={() => handleDrop(index)}
             onClick={() => props.setCurrentIndex(index)}
             active={index === props.currentIndex}
           >
             <Thumbnail src={video.thumbnail} alt={`Video ${video.id}`} />
-            <CommonButton
-              onClick={e => {
-                e.stopPropagation();
-                handleRemove(index);
-              }}
-              color={ButtonColor.DARKGRAY}
-              borderRadius="100px"
-              padding="5px"
-            >
-              <img src={Trashcan} />
-            </CommonButton>
+            <div>
+              <CommonButton
+                onClick={e => {
+                  e.stopPropagation();
+                  handleMoveUp(index);
+                }}
+                color={ButtonColor.DARKGRAY}
+                borderradius="100px"
+                padding="5px"
+              >
+                <img src={ArrowUp} alt="Move Up" />
+              </CommonButton>
+              <CommonButton
+                onClick={e => {
+                  e.stopPropagation();
+                  handleMoveDown(index);
+                }}
+                color={ButtonColor.DARKGRAY}
+                borderradius="100px"
+                padding="5px"
+              >
+                <img src={ArrowDown} alt="Move Down" />
+              </CommonButton>
+              <CommonButton
+                onClick={e => {
+                  e.stopPropagation();
+                  handleRemove(index);
+                }}
+                color={ButtonColor.DARKGRAY}
+                borderradius="100px"
+                padding="5px"
+              >
+                <img src={Trashcan} alt="Remove" />
+              </CommonButton>
+            </div>
           </VideoItem>
         ))}
       </Wrapper>
       <InputContainer>
         <SearchInput
           type="text"
-          placeholder="url을 넣어주세요"
+          placeholder="URL을 입력하세요"
           value={inputUrl}
           onChange={e => setInputUrl(e.target.value)}
         />
