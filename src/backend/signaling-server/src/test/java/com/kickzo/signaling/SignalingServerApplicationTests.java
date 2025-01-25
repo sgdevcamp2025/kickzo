ac62a92a73fff3aed562059a173880e881ede79e
dhwoo <wooda01@naver.com>
2025-01-25 23:39:24 +0900

    [BE] FEAT: 시그널링 서버 초기 세팅 #56

---
+++ src/backend/signaling-server/src/test/java/com/kickzo/signaling/SignalingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kickzo.signaling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
