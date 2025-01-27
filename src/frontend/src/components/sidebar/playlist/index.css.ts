60c207a1cbc5ccd2343a2216ee5a7ea39b021f11
juwon5272 <jackson5272@naver.com>
2025-01-27 17:06:33 +0900

    [FE] FEAT: 플레이리스트에서 순서변경 #47

--- src/frontend/src/components/sidebar/playlist/index.css.ts
+++ src/frontend/src/components/sidebar/playlist/index.css.ts
@@ -51,3 +51,85 @@ export const SearchInput = styled.input`
   border-radius: 5px;
   padding: 0 10px;
 `;
+
+export const PreviewContainer = styled.div`
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  overflow: hidden;
+  &:hover .overlay {
+    opacity: 1;
+    cursor: pointer;
+  }
+`;
+
+export const PreviewImg = styled.img`
+  height: 80px;
+  display: block;
+  border-radius: 8px;
+  margin-right: 10px;
+`;
+
+export const PreviewInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: white;
+  gap: 10px;
+`;
+
+export const Playlist__Title = styled.div`
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 14.52px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  color: black;
+`;
+
+export const Playlist__Youtuber = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 14.52px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  color: #888888;
+`;
+
+export const PreviewInfo__Title = styled.div`
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 14.52px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+`;
+
+export const PreviewInfo__Youtuber = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 14.52px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  color: #888888;
+`;
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 1.2rem;
+  font-weight: bold;
+  border-radius: 8px;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+`;
