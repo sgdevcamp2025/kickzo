cb23899489efa1933ecb9d25fd7537baaa66838a
dhwoo <wooda01@naver.com>
2025-01-30 02:38:02 +0900

    [BE] FEAT: EventListener 기능 구현 #56

---
+++ src/backend/signaling-server/src/main/java/com/kickzo/signaling/event/WebSocketEventListener.java
@@ -0,0 +1,36 @@
+package com.kickzo.signaling.event;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.kickzo.signaling.service.RoomManager;
+import com.kickzo.signaling.service.UserRegistry;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+	private final UserRegistry userRegistry;
+	private final RoomManager roomManager;
+
+	@EventListener
+	public void handleSessionConnectEvent(SessionConnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String sessionId = headerAccessor.getSessionId();
+		log.info("WebSocket connected with session ID {}", sessionId);
+	}
+
+	@EventListener
+	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String sessionId = headerAccessor.getSessionId();
+		log.info("WebSocket disconnected with session ID {}", sessionId);
+	}
+}
