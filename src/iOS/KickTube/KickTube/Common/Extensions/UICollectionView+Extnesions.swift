c0d537f8986933bf7093ea5946fd97814adac57f
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2025-01-30 23:45:39 +0900

    [iOS] DESIGN: CollectionViewCell 여백 수정 #36

--- src/iOS/KickTube/KickTube/Common/Extensions/UICollectionView+Extnesions.swift
+++ src/iOS/KickTube/KickTube/Common/Extensions/UICollectionView+Extnesions.swift
@@ -11,7 +11,7 @@ extension UICollectionViewLayout {
     static func homeCollectionViewSection() -> NSCollectionLayoutSection {
         let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: size)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
         
         let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(ComponentSize.homeCollectionViewCell.size.width), heightDimension: .absolute(ComponentSize.homeCollectionViewCell.size.height))
         let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
