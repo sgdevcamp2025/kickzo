07124564a17be031532f618dd86f4563ad234aa2
juwon5272 <jackson5272@naver.com>
2025-02-01 13:03:48 +0900

    [FE] REFACTOR: 코드 정리

--- src/frontend/src/components/common/BigProfile/index.tsx
+++ src/frontend/src/components/common/BigProfile/index.tsx
@@ -1,3 +1,18 @@
+import { RoleChangeButton } from '@/components/RoleChangeButton';
+import { CommonButton } from '@/components/common/Button';
+import { IconButton } from '@/components/IconButton';
+
+import { UserRole } from '@/types/enums/UserRole';
+import { ButtonColor } from '@/types/enums/ButtonColor';
+import { SidebarType } from '@/types/enums/SidebarType';
+
+import MicrophoneOn from '@/assets/img/MicrophoneOn.svg';
+import MicrophoneOffRed from '@/assets/img/MicrophoneOffRed.svg';
+import HeadphoneOn from '@/assets/img/HeadphoneOn.svg';
+import HeadphoneOffRed from '@/assets/img/HeadphoneOffRed.svg';
+import AddUser from '@/assets/img/AddUser.svg';
+import Check from '@/assets/img/Check.svg';
+
 import {
   Container,
   Profile,
@@ -7,44 +22,33 @@ import {
   Profile__Nickname,
   ButtonContainer,
 } from './index.css';
-import { UserRole } from '@/types/enums/UserRole';
-import { ButtonColor } from '@/types/enums/ButtonColor';
-import { SidebarType } from '@/types/enums/SidebarType';
-
-import { RoleChangeButton } from '@/components/RoleChangeButton';
-import { CommonButton } from '@/components/common/Button';
-import { IconButton } from '@/components/IconButton';
-
-import MicrophoneOn from '@/assets/img/MicrophoneOn.svg';
-import MicrophoneOffRed from '@/assets/img/MicrophoneOffRed.svg';
-import HeadphoneOn from '@/assets/img/HeadphoneOn.svg';
-import HeadphoneOffRed from '@/assets/img/HeadphoneOffRed.svg';
-import AddUser from '@/assets/img/AddUser.svg';
-import Check from '@/assets/img/Check.svg';
 
 interface IBigProfile {
-  userId: string;
+  userId: number;
   userRole: UserRole;
   myRole: UserRole;
   sidebarType: SidebarType;
 }
 
+// TODO - userId를 이용해서 해당 데이터를 API 콜로 받아오는 것을 변경하기
 const detailProfile = {
   imgUrl:
     'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA3MjVfMTQ5%2FMDAxNTk1Njc4MzEyNzA4.knqIC64twrLoZDviHrAUSrEbgtxNp8h4nGsT-4mrWgkg.VImfsqV3F5GqyCPCIN4Xfid4TpUXQkljevfhuX_HK4gg.JPEG.haha9558%2FIMG_0114.JPG&type=a340',
   nickname: '이노',
   introduce: '저는 이제 집으로 갑니다',
 };
+
 export const BigProfile = (props: IBigProfile) => {
   const renderBtnContainer = () => {
-    if (props.myRole === UserRole.OWNER && props.sidebarType === SidebarType.VoiceChat) {
+    if (
+      props.myRole === UserRole.OWNER &&
+      props.userRole !== UserRole.OWNER &&
+      props.sidebarType === SidebarType.VoiceChat
+    ) {
       return (
         <>
-          <RoleChangeButton
-            myRole={props.myRole}
-            userRole={props.userRole}
-            text="권한"
-          ></RoleChangeButton>
+          {/* TODO - 버튼 누를시 역할 변경 요청 보내기 */}
+          <RoleChangeButton myRole={props.myRole} userRole={props.userRole} text="권한" />
           <CommonButton
             color={ButtonColor.RED}
             onClick={() => {
@@ -74,14 +78,10 @@ export const BigProfile = (props: IBigProfile) => {
         </>
       );
     }
-    if (props.myRole === UserRole.OWNER) {
+    if (props.myRole === UserRole.OWNER && props.userRole !== UserRole.OWNER) {
       return (
         <>
-          <RoleChangeButton
-            myRole={props.myRole}
-            userRole={props.userRole}
-            text="권한"
-          ></RoleChangeButton>
+          <RoleChangeButton myRole={props.myRole} userRole={props.userRole} text="권한" />
           <CommonButton
             color={ButtonColor.RED}
             onClick={() => {
@@ -119,7 +119,6 @@ export const BigProfile = (props: IBigProfile) => {
         <Profile__Nickname>{detailProfile.nickname}</Profile__Nickname>
         <p>{detailProfile.introduce}</p>
       </Profile>
-
       <ButtonContainer>{renderBtnContainer()}</ButtonContainer>
     </Container>
   );
