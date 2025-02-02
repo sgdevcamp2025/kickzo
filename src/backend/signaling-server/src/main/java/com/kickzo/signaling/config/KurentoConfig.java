3eb1474cc26a00f6730f4e600c6679273f495ffb
dhwoo <wooda01@naver.com>
2025-02-02 16:15:34 +0900

    [BE] REFACTOR: Kurento URI를 환경 변수에서 주입하도록 변경 #56

--- src/backend/signaling-server/src/main/java/com/kickzo/signaling/config/KurentoConfig.java
+++ src/backend/signaling-server/src/main/java/com/kickzo/signaling/config/KurentoConfig.java
@@ -1,16 +1,18 @@
 package com.kickzo.signaling.config;
 
 import org.kurento.client.KurentoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KurentoConfig {
 
-	private static final String KURENTO_URI = "ws://localhost:8888/kurento";
+	@Value("${KURENTO_URI}")  // 환경 변수에서 KMS 주소 가져오기
+	private String kurentoUri;
 
 	@Bean
 	public KurentoClient kurentoClient() {
-		return KurentoClient.create(KURENTO_URI);
+		return KurentoClient.create(kurentoUri);
 	}
 }
