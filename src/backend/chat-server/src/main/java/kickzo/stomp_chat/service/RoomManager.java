321318e7563900a844f55a36fd2469e1fe093451
yoo-chris <chris4929@naver.com>
2025-02-04 13:14:26 +0900

    [BE] feat: produce websocket connection to kafka #90

--- src/backend/chat-server/src/main/java/kickzo/stomp_chat/service/RoomManager.java
+++ src/backend/chat-server/src/main/java/kickzo/stomp_chat/service/RoomManager.java
@@ -1,8 +1,11 @@
 package kickzo.stomp_chat.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -13,11 +16,17 @@ public class RoomManager {
 
     private final Map<String, Set<String>> rooms = new HashMap<>();
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
     private static final String TOPIC_NAME = "chatting"; // Kafka 토픽 이름
+    private static final String TOPIC_CONNECTION = "connection";
+
+    @Value("${server.port}")  // 현재 서버 포트 번호 주입
+    private String serverPort;
 
     public RoomManager(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = new ObjectMapper();
     }
 
     /**
@@ -26,6 +35,8 @@ public class RoomManager {
     public void joinRoom(String roomId, String userId) {
         rooms.computeIfAbsent(roomId, k -> new HashSet<>()).add(userId);
         System.out.println("User " + userId + " joined room " + roomId);
+
+        sendConnection(userId, roomId, "JOIN");
     }
 
     /**
@@ -40,6 +51,8 @@ public class RoomManager {
             }
         }
         System.out.println("User " + userId + " left room " + roomId);
+
+        sendConnection(userId, roomId, "LEAVE");
     }
 
     /**
@@ -57,5 +70,23 @@ public class RoomManager {
                 roomId, userId, content);
         kafkaTemplate.send(TOPIC_NAME, message);
         System.out.println("Kafka message sent: " + message);
+    }
+
+    //kafka에 connection 보내기
+    public void sendConnection(String userId, String roomId, String eventType) {
+        try {
+            Map<String, String> message = new HashMap<>();
+            message.put("userId", userId);
+            message.put("roomId", roomId);
+            message.put("eventType", eventType);   //Join or Leave
+            message.put("serverPort", serverPort);
+            message.put("timestamp", Instant.now().toString());
+
+            String jsonMessage = objectMapper.writeValueAsString(message);
+            kafkaTemplate.send(TOPIC_CONNECTION, jsonMessage);
+            System.out.println("Kafka connection event sent: " + jsonMessage);
+        } catch (Exception e) {
+            System.err.println("Faile to send Kafka connection event: " + e.getMessage());
+        }
     }
 }
