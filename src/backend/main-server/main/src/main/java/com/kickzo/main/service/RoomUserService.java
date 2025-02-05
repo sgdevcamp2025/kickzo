2ddf8fb2fac6a02f982f9dbe838a4f3249e0a46b
dhwoo <wooda01@naver.com>
2025-02-05 13:07:48 +0900

    [BE] FEAT: role 변경 이벤트 구현 #85

---
+++ src/backend/main-server/main/src/main/java/com/kickzo/main/service/RoomUserService.java
@@ -0,0 +1,46 @@
+package com.kickzo.main.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kickzo.main.entity.RoomUser;
+import com.kickzo.main.entity.RoomUserId;
+import com.kickzo.main.exception.CustomErrorCode;
+import com.kickzo.main.exception.CustomException;
+import com.kickzo.main.repository.RoomUserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RoomUserService {
+
+	private final RoomUserRepository roomUserRepository;
+	private final KafkaProducerService kafkaProducerService;
+
+	@Transactional
+	public void changeUserRole(Long userId, Long roomId, Long targetUserId, int newRole) {
+		RoomUserId requesterId = new RoomUserId(roomId, userId);
+		Optional<RoomUser> requester = roomUserRepository.findById(requesterId);
+
+		if (requester.isEmpty()) {
+			throw new CustomException(CustomErrorCode.USER_NOT_FOUND);
+		}
+
+		if (requester.get().getRole() == 2) {
+			throw new CustomException(CustomErrorCode.INVALID_ACCESS_ROLE);
+		}
+
+		RoomUserId targetId = new RoomUserId(roomId, targetUserId);
+		Optional<RoomUser> targetUser = roomUserRepository.findById(targetId);
+
+		if (targetUser.isEmpty()) {
+			throw new CustomException(CustomErrorCode.USER_NOT_FOUND);
+		}
+
+		targetUser.get().setRole(newRole);
+		kafkaProducerService.sendRoleChangeEvent(roomId, targetUserId, newRole);
+	}
+}
