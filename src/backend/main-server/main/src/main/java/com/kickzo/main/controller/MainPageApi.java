9acf922161cb793bc43592ce8564b9a5d14f781c
dhwoo <wooda01@naver.com>
2025-02-10 14:55:40 +0900

    [BE] FIX: MainPageApi 필요없는 annotation 삭제 #103

--- src/backend/main-server/main/src/main/java/com/kickzo/main/controller/MainPageApi.java
+++ src/backend/main-server/main/src/main/java/com/kickzo/main/controller/MainPageApi.java
@@ -3,8 +3,6 @@ package com.kickzo.main.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +13,6 @@ import com.kickzo.main.dto.response.CreateRoomResponseDto;
 import com.kickzo.main.dto.response.RoomResponseDto;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
