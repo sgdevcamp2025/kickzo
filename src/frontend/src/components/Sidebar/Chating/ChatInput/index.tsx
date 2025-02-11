e16b5834bb087e6d241d53e41c747e0dfa4a27f5
juwon5272 <jackson5272@naver.com>
2025-02-11 20:45:20 +0900

    [FE] FEAT: 채팅 입력 및 전송용 Input #123

---
+++ src/frontend/src/components/Sidebar/Chating/ChatInput/index.tsx
@@ -0,0 +1,50 @@
+import { useState, useRef } from 'react';
+import { CommonInput } from '@/components/common/Input';
+import { CommonButton } from '@/components/common/Button';
+import Add from '@/assets/img/Add.svg';
+import { ButtonColor } from '@/types/enums/ButtonColor';
+import { InputContainer } from './index.css';
+
+interface IChatInput {
+  onSendMessage: (message: string) => void;
+}
+
+export const ChatInput = (props: IChatInput) => {
+  const [message, setMessage] = useState('');
+  const enterKeyProcessed = useRef(false);
+
+  const handleSend = () => {
+    if (message.trim() !== '') {
+      props.onSendMessage(message);
+      setMessage('');
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      if (e.nativeEvent.isComposing || enterKeyProcessed.current) return;
+      enterKeyProcessed.current = true;
+
+      handleSend();
+
+      setTimeout(() => {
+        enterKeyProcessed.current = false;
+      }, 0);
+    }
+  };
+
+  return (
+    <InputContainer>
+      <CommonButton color={ButtonColor.TRANSPARENT} borderradius="20px" onClick={handleSend}>
+        <img src={Add} alt="Add" />
+      </CommonButton>
+      <CommonInput
+        placeholder="메시지 보내기"
+        design={1}
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    </InputContainer>
+  );
+};
