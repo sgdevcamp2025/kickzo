fbf9124915b7b23430c6ba1603b9668704302f8c
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2025-02-12 14:16:17 +0900

    [iOS] FEAT: Date 변환 메서드 구현 #148

---
+++ src/iOS/KickTube/KickTube/Common/Extensions/Int+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Int+Extensions.swift
+//  KickTube
+//
+//  Created by 김수경 on 2/11/25.
+//
+
+import Foundation
+
+extension Int {
+    var toDate: Date {
+        return Date(timeIntervalSince1970: TimeInterval(self))
+    }
+}
