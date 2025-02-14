2a09b562c9c80a20fd647844b42795c47cafb646
juwon5272 <jackson5272@naver.com>
2025-02-14 09:48:32 +0900

    [FE] REFACTOR: switch문 객체 리터럴 방식으로 변경 #161

--- src/frontend/src/components/Sidebar/index.tsx
+++ src/frontend/src/components/Sidebar/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { ChatBox } from './Chating';
 import { MemberList } from './MemberList';
 import { Playlist } from './Playlist';
+import { VoiceChat } from './VoiceChat';
 import SidebarChat from '@/assets/img/SidebarChat.svg';
 import SidebarPlaylist from '@/assets/img/SidebarPlaylist.svg';
 import SidebarVoicechat from '@/assets/img/SidebarVoicechat.svg';
@@ -12,19 +13,16 @@ import { SidebarType } from '@/types/enums/SidebarType';
 export const Sidebar = () => {
   const [interfaceType, setInterfaceType] = useState<SidebarType>(SidebarType.CHAT);
 
+  const contentComponents = {
+    [SidebarType.CHAT]: ChatBox,
+    [SidebarType.PLAYLIST]: Playlist,
+    [SidebarType.VOICECHAT]: VoiceChat,
+    [SidebarType.MEMBER]: MemberList,
+  };
+
   const renderContent = () => {
-    switch (interfaceType) {
-      case SidebarType.CHAT:
-        return <ChatBox />;
-      case SidebarType.PLAYLIST:
-        return <Playlist />;
-      case SidebarType.VOICECHAT:
-        return <MemberList sidebarType={SidebarType.VOICECHAT} />;
-      case SidebarType.MEMBER:
-        return <MemberList sidebarType={SidebarType.MEMBER} />;
-      default:
-        return null;
-    }
+    const Component = contentComponents[interfaceType];
+    return Component ? <Component /> : null;
   };
 
   return (
