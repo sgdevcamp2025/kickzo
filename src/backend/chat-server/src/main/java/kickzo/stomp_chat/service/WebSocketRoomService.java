9a2bdff7fba1f52ef9cf985db1e1afba7859046b
dhwoo <wooda01@naver.com>
2025-02-15 19:48:07 +0900

    [BE] FIX: RoomManager를 WebSocketRoomService로 이름 변경 및 기능 수정 #174

---
+++ src/backend/chat-server/src/main/java/kickzo/stomp_chat/service/WebSocketRoomService.java
@@ -0,0 +1,51 @@
+package kickzo.stomp_chat.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import kickzo.stomp_chat.dto.ChatMessage;
+import kickzo.stomp_chat.dto.ConnectionEvent;
+import kickzo.stomp_chat.enums.UserEventType;
+import kickzo.stomp_chat.repository.KafkaRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class WebSocketRoomService {
+
+	private final Map<Long, Set<Long>> rooms = new HashMap<>();
+	private final KafkaRepository kafkaRepository;
+
+	@Value("${server.port}")  // 현재 서버 포트 번호 주입
+	private String serverPort;
+
+	/**
+	 * 특정 페이지에서 사용자 제거
+	 */
+	public void leavePage(long userId) {
+		log.info("User {} leave service", userId);
+		sendConnection(userId, UserEventType.LEAVE);
+	}
+
+	/**
+	 * 방에 메시지 전송
+	 */
+	public void handleMessageSend(long roomId, long userId, String content, String msg) {
+		ChatMessage chatMessage = new ChatMessage(roomId, userId, content, msg);
+		kafkaRepository.sendChatMessage(chatMessage);
+	}
+
+	/**
+	 * Kafka에 사용자 연결 상태 전송
+	 */
+	public void sendConnection(long userId, UserEventType userEventType) {
+		ConnectionEvent event = new ConnectionEvent(userId, userEventType, serverPort);
+		kafkaRepository.sendConnectionEvent(event);
+	}
+}
