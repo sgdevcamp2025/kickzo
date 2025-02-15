0b1144cc0c922356b31faae57980bcc1b4cf0624
dhwoo <wooda01@naver.com>
2025-02-15 19:53:51 +0900

    [BE] FIX: 중복 코드 주석 처리 #174

--- src/backend/chat-server/src/main/java/kickzo/stomp_chat/event/WebSocketEventListener.java
+++ src/backend/chat-server/src/main/java/kickzo/stomp_chat/event/WebSocketEventListener.java
@@ -1,28 +1,29 @@
 package kickzo.stomp_chat.event;
 
-import kickzo.stomp_chat.service.RoomManager;
+import kickzo.stomp_chat.service.WebSocketRoomService;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+@Slf4j
 @Component
 public class WebSocketEventListener implements WebSocketMessageBrokerConfigurer {
 
-    @Autowired
-    private RoomManager roomManager;
+	@Autowired
+	private WebSocketRoomService webSocketRoomService;
 
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectEvent event) {
         // 새로운 WebSocket 연결 발생 시
         SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
         String sessionId = headerAccessor.getSessionId();
-        System.out.println("New WebSocket connection: " + sessionId);
+        log.info("New WebSocket connection: " + sessionId);
     }
 
     @EventListener
@@ -30,24 +31,23 @@ public class WebSocketEventListener implements WebSocketMessageBrokerConfigurer 
         // WebSocket 연결 해제 시
         SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
         Long userId = (Long) headerAccessor.getSessionAttributes().get("userId");  // 형 변환
-        long roomId = 1L; // 예시로 단일 방 사용
 
         if (userId != null) {
-            roomManager.leaveRoom(roomId, userId);
+            webSocketRoomService.leavePage(userId);
         }
-        System.out.println("WebSocket disconnected: " + userId);
+        log.info("WebSocket disconnected: " + userId);
     }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 메세지 브로커가 처리할 목적지 지정
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // STOMP 엔드포인트 등록 (WebSocket 접속 경로)
-        registry.addEndpoint("/stomp-chat").withSockJS();
-    }
+    // @Override
+    // public void configureMessageBroker(MessageBrokerRegistry registry) {
+    //     // 메세지 브로커가 처리할 목적지 지정
+    //     registry.enableSimpleBroker("/topic");
+    //     registry.setApplicationDestinationPrefixes("/app");
+    // }
+    //
+    // @Override
+    // public void registerStompEndpoints(StompEndpointRegistry registry) {
+    //     // STOMP 엔드포인트 등록 (WebSocket 접속 경로)
+    //     registry.addEndpoint("/stomp-chat").withSockJS();
+    // }
 }
