27b38b0826117cccfda32119bbe5103a80870d7f
dhwoo <wooda01@naver.com>
2025-02-15 19:45:13 +0900

    [BE] FEAT: Kafka로 정보 보내는 repository 추가 #174

---
+++ src/backend/chat-server/src/main/java/kickzo/stomp_chat/repository/KafkaRepository.java
@@ -0,0 +1,41 @@
+package kickzo.stomp_chat.repository;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kickzo.stomp_chat.dto.ChatMessage;
+import kickzo.stomp_chat.dto.ConnectionEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaRepository {
+	private final KafkaTemplate<String, String> kafkaTemplate;
+	private final ObjectMapper objectMapper;
+	private static final String TOPIC_CHAT = "chatting";
+	private static final String TOPIC_CONNECTION = "connection";
+
+	public void sendConnectionEvent(ConnectionEvent event) {
+		try {
+			String jsonMessage = objectMapper.writeValueAsString(event);
+			kafkaTemplate.send(TOPIC_CONNECTION, jsonMessage);
+			log.info("Kafka connection event sent: {}", jsonMessage);
+		} catch (Exception e) {
+			log.error("Failed to send Kafka connection event: {}", e.getMessage());
+		}
+	}
+
+	public void sendChatMessage(ChatMessage chatMessage) {
+		try {
+			String jsonMessage = objectMapper.writeValueAsString(chatMessage);
+			kafkaTemplate.send(TOPIC_CHAT, jsonMessage);
+			log.info("Kafka message sent: {}", jsonMessage);
+		} catch (Exception e) {
+			log.error("Failed to send Kafka chat message: {}", e.getMessage());
+		}
+	}
+}
