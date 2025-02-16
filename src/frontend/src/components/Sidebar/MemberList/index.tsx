4645b96442833e7d0807612a2fc4ec8c70821994
juwon5272 <jackson5272@naver.com>
2025-02-16 12:29:07 +0900

    [FE] REFACTOR: red-black tree 따로 분리 #161

--- src/frontend/src/components/Sidebar/MemberList/index.tsx
+++ src/frontend/src/components/Sidebar/MemberList/index.tsx
@@ -11,14 +11,13 @@ import { useUserList, IUser } from '@/hooks/utils/useUserList';
 import { Container, UserList, ProfileWrapper } from './index.css';
 
 export const MemberList = () => {
-  const { users, addUser, removeUser } = useUserList();
+  const { users, addUser } = useUserList();
   const [activeProfile, setActiveProfile] = useState<number | null>(null);
-  const [nickname, setNickname] = useState('');
+
   const handleProfileClick = (id: number) => {
     setActiveProfile(prevId => (prevId === id ? null : id));
   };
 
-  // 테스트용: 임의로 유저 만들어서 추가
   const handleAddUser = () => {
     const newUser: IUser = {
       id: Date.now(),
@@ -43,29 +42,17 @@ export const MemberList = () => {
               />
             </div>
             {activeProfile === member.id && (
-              <div className="profile-detail active">
-                <ProfileDetail
-                  userId={member.id}
-                  userRole={member.role}
-                  myRole={UserRole.CREATOR}
-                  sidebarType={SidebarType.MEMBER}
-                />
-              </div>
+              <ProfileDetail
+                userId={member.id}
+                userRole={member.role}
+                myRole={UserRole.CREATOR}
+                sidebarType={SidebarType.MEMBER}
+              />
             )}
           </ProfileWrapper>
         ))}
       </UserList>
-
-      {/* 테스트용 버튼 start*/}
       <button onClick={handleAddUser}>랜덤 유저 추가</button>
-      <input
-        type="text"
-        value={nickname}
-        onChange={e => setNickname(e.target.value)}
-        placeholder="삭제할 유저"
-        style={{ marginLeft: '10px', padding: '5px', width: '120px' }}
-      />
-      <button onClick={() => removeUser(nickname)}>삭제</button>
       <MemberListFooter sidebarType={SidebarType.MEMBER} />
     </Container>
   );
--- src/frontend/src/hooks/utils/useUserList.ts
+++ src/frontend/src/hooks/utils/useUserList.ts
@@ -1,158 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 import { memberListTest } from '@/assets/data/memberListTest';
+import { RedBlackTree } from '@/hooks/utils/RedBlackTree';
 
 export interface IUser {
   id: number;
   role: number;
   nickname: string;
   profileImg: string;
-}
-
-type Color = 'red' | 'black';
-
-class RBNode<T> {
-  data: T;
-  color: Color;
-  left: RBNode<T> | null = null;
-  right: RBNode<T> | null = null;
-  parent: RBNode<T> | null = null;
-  constructor(data: T, color: Color = 'red') {
-    this.data = data;
-    this.color = color;
-  }
-}
-
-class RedBlackTree<T extends { nickname: string }> {
-  root: RBNode<T> | null = null;
-  compare: (a: T, b: T) => number;
-
-  constructor(compare: (a: T, b: T) => number) {
-    this.compare = compare;
-  }
-
-  insert(data: T) {
-    const newNode = new RBNode(data);
-    let y: RBNode<T> | null = null;
-    let x = this.root;
-
-    while (x !== null) {
-      y = x;
-      if (this.compare(newNode.data, x.data) < 0) {
-        x = x.left;
-      } else {
-        x = x.right;
-      }
-    }
-
-    newNode.parent = y;
-    if (y === null) {
-      this.root = newNode;
-    } else if (this.compare(newNode.data, y.data) < 0) {
-      y.left = newNode;
-    } else {
-      y.right = newNode;
-    }
-
-    newNode.color = 'red';
-    this.fixInsert(newNode);
-  }
-
-  private fixInsert(z: RBNode<T>) {
-    while (z.parent && z.parent.color === 'red') {
-      if (z.parent === z.parent.parent?.left) {
-        const y = z.parent.parent.right;
-        if (y && y.color === 'red') {
-          z.parent.color = 'black';
-          y.color = 'black';
-          z.parent.parent.color = 'red';
-          z = z.parent.parent;
-        } else {
-          if (z === z.parent.right) {
-            z = z.parent;
-            this.leftRotate(z);
-          }
-          z.parent!.color = 'black';
-          if (z.parent!.parent) {
-            z.parent!.parent.color = 'red';
-            this.rightRotate(z.parent!.parent);
-          }
-        }
-      } else {
-        const y = z.parent.parent?.left;
-        if (y && y.color === 'red') {
-          z.parent.color = 'black';
-          y.color = 'black';
-          z.parent.parent!.color = 'red';
-          z = z.parent.parent!;
-        } else {
-          if (z === z.parent.left) {
-            z = z.parent;
-            this.rightRotate(z);
-          }
-          z.parent!.color = 'black';
-          if (z.parent!.parent) {
-            z.parent!.parent.color = 'red';
-            this.leftRotate(z.parent!.parent);
-          }
-        }
-      }
-    }
-    if (this.root) {
-      this.root.color = 'black';
-    }
-  }
-
-  private leftRotate(x: RBNode<T>) {
-    const y = x.right;
-    if (!y) return;
-    x.right = y.left;
-    if (y.left) y.left.parent = x;
-    y.parent = x.parent;
-    if (!x.parent) {
-      this.root = y;
-    } else if (x === x.parent.left) {
-      x.parent.left = y;
-    } else {
-      x.parent.right = y;
-    }
-    y.left = x;
-    x.parent = y;
-  }
-
-  private rightRotate(y: RBNode<T>) {
-    const x = y.left;
-    if (!x) return;
-    y.left = x.right;
-    if (x.right) x.right.parent = y;
-    x.parent = y.parent;
-    if (!y.parent) {
-      this.root = x;
-    } else if (y === y.parent.right) {
-      y.parent.right = x;
-    } else {
-      y.parent.left = x;
-    }
-    x.right = y;
-    y.parent = x;
-  }
-
-  deleteByName(nickname: string) {
-    const users = this.inOrderTraversal().filter(user => user.nickname !== nickname);
-    this.root = null;
-    users.forEach(user => this.insert(user));
-  }
-
-  inOrderTraversal(): T[] {
-    const result: T[] = [];
-    const traverse = (node: RBNode<T> | null) => {
-      if (!node) return;
-      traverse(node.left);
-      result.push(node.data);
-      traverse(node.right);
-    };
-    traverse(this.root);
-    return result;
-  }
 }
 
 const compareUsers = (a: IUser, b: IUser): number => {
@@ -194,14 +48,5 @@ export const useUserList = () => {
     localStorage.setItem('userList', JSON.stringify(updatedUsers));
   };
 
-  // TODO: 추후 유저의 ID를 이용해서 삭제하는 기능으로 변경 예정
-  const removeUser = (nickname: string) => {
-    if (!treeRef.current) return;
-    treeRef.current.deleteByName(nickname);
-    const updatedUsers = treeRef.current.inOrderTraversal();
-    setUsers(updatedUsers);
-    localStorage.setItem('userList', JSON.stringify(updatedUsers));
-  };
-
-  return { users, addUser, removeUser };
+  return { users, addUser };
 };
