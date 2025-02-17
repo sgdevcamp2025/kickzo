83c5b4b044f63fbb232a70897b0174c485748635
juwon5272 <jackson5272@naver.com>
2025-02-17 11:14:37 +0900

    [FE] DELETE: 불필요한 파일 삭제

--- src/frontend/src/types/enums/ProfileDetailType.ts
+++
@@ -1,9 +0,0 @@
-import { SidebarType } from "./SidebarType";
-
-export enum ProfileDetailType {
-  CHAT = SidebarType.CHAT,
-  PLAYLIST = SidebarType.PLAYLIST,
-  VOICECHAT = SidebarType.VOICECHAT,
-  MEMBER = SidebarType.MEMBER,
-  EDIT = 4,
-}
