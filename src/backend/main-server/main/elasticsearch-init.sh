dd896dd5eb8e66696190488d1516cdff0ee24b25
dhwoo <wooda01@naver.com>
2025-02-18 19:40:00 +0900

    [BE] FEAT: Kibana 설정 추가 #198

--- src/backend/main-server/main/elasticsearch-init.sh
+++ src/backend/main-server/main/elasticsearch-init.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 echo "⏳ Waiting for Elasticsearch to be ready..."
-until curl -s -u elastic:${ELASTIC_PASSWORD} "http://localhost:9200/_cluster/health" | grep -q '"status":"green"\|"status":"yellow"'; do
+until curl -s -u elastic:test123 "http://localhost:9200/_cluster/health" | grep -q '"status":"green"\|"status":"yellow"'; do
   sleep 5
 done
 
-echo "✅ Elasticsearch is ready. Creating user and roles..."
+echo "✅ Elasticsearch is ready. Creating and roles..."
 
 # kickzo_role 생성
 curl -X PUT "http://localhost:9200/_security/role/kickzo_role" \
 -H "Content-Type: application/json" \
--u elastic:${ELASTIC_PASSWORD} \
+-u elastic:test123 \
 -d '{
   "cluster": ["all"],
   "indices": [
@@ -25,7 +25,7 @@ curl -X PUT "http://localhost:9200/_security/role/kickzo_role" \
 # kickzo 사용자 생성 또는 업데이트
 curl -X PUT "http://localhost:9200/_security/user/kickzo" \
 -H "Content-Type: application/json" \
--u elastic:${ELASTIC_PASSWORD} \
+-u elastic:test123 \
 -d '{
   "password": "test123",
   "roles": ["superuser", "kickzo_role"]
--- src/backend/main-server/main/src/main/java/com/kickzo/main/MainApplication.java
+++ src/backend/main-server/main/src/main/java/com/kickzo/main/MainApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @EnableElasticsearchRepositories(basePackages = "com.kickzo.main")
 public class MainApplication {
@@ -11,5 +14,4 @@ public class MainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
-
 }
