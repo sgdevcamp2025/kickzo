0b3f5d3de941e65652b2684cfa1adcea5aeab602
42inshin <inshin@student.42seoul.kr>
2025-02-19 09:17:35 +0900

    [FE] FIX: date format 표시 형식 변경 #193

--- src/frontend/src/utils/dateUtils.ts
+++ src/frontend/src/utils/dateUtils.ts
@@ -1,16 +1,20 @@
 export const formatDateToKorean = (timestamp: string | number | Date): string => {
   const date = new Date(timestamp);
   const today = new Date();
+
   if (date.toDateString() === today.toDateString()) {
     return date.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit', hour12: false });
   } else {
-    return date.toLocaleString('ko-KR', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
+    const isCurrentYear = date.getFullYear() === today.getFullYear();
+    const formattedDate = isCurrentYear
+      ? `${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')}`
+      : `${date.getFullYear()}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')}`;
+
+    const formattedTime = date.toLocaleTimeString('ko-KR', {
       hour: '2-digit',
       minute: '2-digit',
       hour12: false,
     });
+    return `${formattedDate} ${formattedTime}`;
   }
 };
