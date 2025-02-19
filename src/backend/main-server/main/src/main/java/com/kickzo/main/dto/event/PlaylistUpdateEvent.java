31fce615dc9b03d0ad8ac597a24364889939f09e
dhwoo <wooda01@naver.com>
2025-02-19 12:47:24 +0900

    [BE] FIX: playlist 변수명 변경 #198

--- src/backend/main-server/main/src/main/java/com/kickzo/main/dto/event/PlaylistUpdateEvent.java
+++ src/backend/main-server/main/src/main/java/com/kickzo/main/dto/event/PlaylistUpdateEvent.java
@@ -13,5 +13,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PlaylistUpdateEvent {
 	private Long roomId;
-	private List<PlaylistItem> playlistJson;
+	private List<PlaylistItem> playlist;
 }
