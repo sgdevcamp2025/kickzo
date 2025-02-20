57db50905ac42986f144887d02d1cec463710b68
juwon5272 <jackson5272@naver.com>
2025-02-20 09:21:12 +0900

    [FE] FEAT: 프로필에 필요한 매개변수 추가 #203

--- src/frontend/src/types/dto/ProfileDetailDto.dto.ts
+++ src/frontend/src/types/dto/ProfileDetailDto.dto.ts
@@ -3,7 +3,11 @@ import { SidebarType } from '../enums/SidebarType';
 
 export interface ProfileDetailDto {
   userId: number;
+  roomId?: number;
   userRole: UserRole;
   myRole: UserRole;
   sidebarType: SidebarType;
+  nickname: string;
+  introduce?: string;
+  imgUrl: string;
 }
