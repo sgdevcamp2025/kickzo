4a3f631fa0f9ac0b970defb35e484d18682227cd
42inshin <inshin@student.42seoul.kr>
2025-02-23 14:04:33 +0900

    [FE] FIX: sentry 초기 설정 파일 분리 #228

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,11 +1,4 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
