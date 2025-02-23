1479277aabbb09e0b7bef65985312e61c2700392
42inshin <inshin@student.42seoul.kr>
2025-02-23 12:48:52 +0900

    [FE] FEAT: Sentry 초기 설정 #228

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
@@ -14,14 +15,25 @@ export default defineConfig({
   plugins: [
     react(),
     visualizer({ open: true }), // 번들 분석 결과 자동 열기
+    sentryVitePlugin({
+      org: 'kickzo',
+      project: 'kicktube',
+      telemetry: false,
+    }),
   ],
+
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
       { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
     ],
   },
+
   define: {
     global: 'window',
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
