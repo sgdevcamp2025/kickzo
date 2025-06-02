6a232776ed7e651db41d8fd9cad8ad49f8c9c05b
dhwoo <wooda01@naver.com>
2025-06-02 17:20:00 +0900

    [BE] REFACTOR: SearchController 로직 SearchService로 이전 #269

--- src/backend/main-server/main/src/main/java/com/kickzo/main/search/controller/SearchController.java
+++ src/backend/main-server/main/src/main/java/com/kickzo/main/search/controller/SearchController.java
@@ -6,57 +6,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import com.kickzo.main.dto.response.RoomResponseDto;
-import com.kickzo.main.repository.RoomRepository;
-import com.kickzo.main.search.document.RoomDocument;
-import com.kickzo.main.search.repository.RoomSearchRepository;
-import com.kickzo.main.search.repository.UserSearchRepository;
-import com.kickzo.main.service.MainPageService;
+import com.kickzo.main.search.service.SearchService;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/search")
 @RequiredArgsConstructor
 public class SearchController {
-
-	private final RoomSearchRepository roomSearchRepository;
-	private final RoomRepository roomRepository;
-	private final UserSearchRepository userSearchRepository;
-	private final MainPageService mainPageService;
+	private final SearchService searchService;
 
 	@GetMapping
 	public Map<String, Object> searchRoomsAndUsers(@RequestParam String keyword) {
 		Map<String, Object> result = new HashMap<>();
-		result.put("rooms", convertToRoomDto(keyword));
-		result.put("users", convertToUserDto(keyword));
+		result.put("rooms", searchService.searchRooms(keyword));
+		result.put("users", searchService.searchUsers(keyword));
 		return result;
 	}
-
-	private List<RoomResponseDto> convertToRoomDto(String keyword) {
-		return roomRepository.findByIdIn(
-			roomSearchRepository.findByTitleContainingAndIsPublic(keyword, true)
-			.stream()
-			.map(RoomDocument::getRoomId)
-			.collect(Collectors.toList())
-		).stream().map(mainPageService::convertToDto)
-			.collect(Collectors.toList());
-	}
-
-	private List<UserResponseDto> convertToUserDto(String keyword) {
-		return userSearchRepository.findByNicknameContaining(keyword)
-			.stream()
-			.map(user -> new UserResponseDto(
-				user.getUserId(),
-				user.getNickname(),
-				user.getStateMessage(),
-				user.getProfileImageUrl()
-			))
-			.toList();
-	}
-
-	public record UserResponseDto(Long userId, String nickname, String stateMessage, String profileImageUrl) {}
 }
